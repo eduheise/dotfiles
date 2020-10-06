dfa3e950f251b20e4be87329e409cd18662b98ac
Eduardo F. J. Heise <eduferreiraj@gmail.com>
2020-10-06 16:51:24 -0300

    update zsh installer
    Signed-off-by: Eduardo F. J. Heise <eduferreiraj@gmail.com>

--- zsh/INSTALL.sh
+++ zsh/INSTALL.sh
@@ -2,5 +2,10 @@ sudo apt install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-echo 'Please install Firamono at: https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fira%20Mono%20Regular%20Nerd%20Font%20Complete.otf?raw=true'
+echo 'Please install Firamono at:' 
+echo 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fira%20Mono%20Regular%20Nerd%20Font%20Complete.otf?raw=true'
+echo
+echo 'Original Medium tutorial at:'
+echo 'https://medium.com/@shivam1/make-your-terminal-beautiful-and-fast-with-zsh-shell-and-powerlevel10k-6484461c6efb'
+
 cp .zshrc ~/
